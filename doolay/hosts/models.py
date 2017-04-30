1c1c5f69db8e74b722e7e271ff658de159ef2636
Yan P Aung <yan.pye.aung@gmail.com>
2017-04-30 14:40:36 -0700

    Revert "Update interests with verbose_name instead of default 'Tags'"
    This reverts commit cee91a2b271353f5520180bf9706d0914cedfbd3.

--- doolay/hosts/models.py
+++ doolay/hosts/models.py
@@ -122,9 +122,7 @@ class HostPage(Page):
         help_text='Host image'
     )
 
-    interests = ClusterTaggableManager(verbose_name='Interests/Hobby',
-                                       help_text='Eg., "Korean Dramas"',
-                                       through=HostInterest, blank=True)
+    interests = ClusterTaggableManager(through=HostInterest, blank=True)
 
     body = StreamField(
         GlobalStreamBlock(), verbose_name="Host's biography", blank=True
