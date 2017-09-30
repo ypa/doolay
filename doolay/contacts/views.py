7099e4c90114fc74792086b1580639bafeb558fe
Yan P Aung <yan.pye.aung@gmail.com>
2017-09-30 13:42:25 -0700

    Fix indentation on contacts views

--- doolay/contacts/views.py
+++ doolay/contacts/views.py
@@ -12,39 +12,39 @@ def contact(request):
     form_class = ContactForm
 
     if request.method == 'POST':
-            form = form_class(data=request.POST)
+        form = form_class(data=request.POST)
 
-            if form.is_valid():
-                contact_name = request.POST.get(
-                    'contact_name'
+        if form.is_valid():
+            contact_name = request.POST.get(
+                'contact_name'
                 , '')
-                contact_email = request.POST.get(
-                    'contact_email'
+            contact_email = request.POST.get(
+                'contact_email'
                 , '')
-                form_content = request.POST.get('content', '')
+            form_content = request.POST.get('content', '')
 
-                # Email the profile with the 
-                # contact information
-                template = get_template('email_template.txt')
-                context = {
-                    'contact_name': contact_name,
-                    'contact_email': contact_email,
-                    'form_content': form_content,
-                }
-                content = template.render(context)
+            # Email the profile with the 
+            # contact information
+            template = get_template('email_template.txt')
+            context = {
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            }
+            content = template.render(context)
 
-                email = EmailMessage(
-                    subject="Inquiry from %s" % contact_email,
-                    body=content,
-                    from_email=contact_email,
-                    to=[settings.DEFAULT_TO_EMAIL],
-                    headers={'Reply-To': contact_email},
-                    cc=(settings.DEFAULT_TO_EMAIL,)
-                )
+            email = EmailMessage(
+                subject="Inquiry from %s" % contact_email,
+                body=content,
+                from_email=contact_email,
+                to=[settings.DEFAULT_TO_EMAIL],
+                headers={'Reply-To': contact_email},
+                cc=(settings.DEFAULT_TO_EMAIL,)
+            )
 
-                email.send()
-                redirect_url = request.META.get('HTTP_REFERER', '/')
-                return HttpResponseRedirect(redirect_url)
+            email.send()
+            redirect_url = request.META.get('HTTP_REFERER', '/')
+            return HttpResponseRedirect(redirect_url)
 
     return render(request, 'contacts.html', {
         'form': form_class,
