a880eaadbda3449a0041a1431df992eaf6b01bf9
Yan P Aung <yan.pye.aung@gmail.com>
2018-01-27 05:13:30 -0800

    Show only live experiences on place page

--- doolay/places/templatetags/place_tags.py
+++ doolay/places/templatetags/place_tags.py
@@ -4,5 +4,5 @@ register = template.Library()
 @register.inclusion_tag('tags/experiences_grid.html')
 def experiences_grid(place_page):
     return {
-        'experiences': place_page.experiences.all(),
+        'experiences': place_page.experiences.all().live(),
     }
