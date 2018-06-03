84566ee35a5a682e37932ac825e5e72dd214192f
Yan P Aung <yan.pye.aung@gmail.com>
2018-06-03 14:29:16 -0700

    Add command to backfill Booking objects

---
+++ doolay/management/commands/backfill_bookings.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from doolay.experiences.models import ExperiencePage
+from doolay.bookings.models import Booking
+
+
+class Command(BaseCommand):
+	"""
+	Command to back-fill Booking objects for all ExperiencePage objects
+	that don't have one associated.
+	"""
+
+	def handle(self, **options):
+		experience_pages = ExperiencePage.objects.filter(booking__isnull=True)
+
+		for experience_page in experience_pages:
+			Booking.objects.create(experience_page=experience_page)
+
+		return None
