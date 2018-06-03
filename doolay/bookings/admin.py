06432bd32b08391230fb554b6ffcc97c64a4cea5
Yan P Aung <yan.pye.aung@gmail.com>
2018-06-03 15:58:28 -0700

    Add booking to the list display of the booking slot admin

--- doolay/bookings/admin.py
+++ doolay/bookings/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from doolay.bookings.models import BookingSlot
 
 class BookingSlotAdmin(admin.ModelAdmin):
-    list_display = ['start', 'end', 'repeat', 'repeat_until', 'notes']
+    list_display = ['booking', 'start', 'end', 'repeat', 'repeat_until', 'notes']
     search_fields = ('booking__experience_page__title',)
 
 admin.site.register(BookingSlot, BookingSlotAdmin)
