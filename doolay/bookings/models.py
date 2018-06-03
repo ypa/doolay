ed13d7a788f00c24f91b8c66de018f90fb451377
Yan P Aung <yan.pye.aung@gmail.com>
2018-06-03 13:32:40 -0700

    Add booking models

--- doolay/bookings/models.py
+++ doolay/bookings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+from eventtools.models import BaseEvent, BaseOccurrence
+from doolay.experiences.models import ExperiencePage
+
+
+class Booking(BaseEvent):
+	experience_page = models.OneToOneField(
+        ExperiencePage,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+
+class BookingSlot(BaseOccurrence):
+	booking = models.ForeignKey(
+		Booking,
+		on_delete=models.CASCADE,
+		related_name='booking_slots',
+		related_query_name='booking_slot',
+	)
