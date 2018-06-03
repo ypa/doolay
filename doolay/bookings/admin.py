885eb1a557aab922fd4c026b3c39612f64e0e1c3
Yan P Aung <yan.pye.aung@gmail.com>
2018-06-03 15:47:39 -0700

    Line format

--- doolay/bookings/admin.py
+++ doolay/bookings/admin.py
@@ -4,7 +4,6 @@ from doolay.bookings.models import BookingSlot
 
 class BookingSlotAdmin(admin.ModelAdmin):
     list_display = ['start', 'end', 'repeat', 'repeat_until', 'notes']
-
     search_fields = ('booking__experience_page__title',)
 
 admin.site.register(BookingSlot, BookingSlotAdmin)
