91e95362da669618c6d1542ceddd302f31de1c97
Yan P Aung <yan.pye.aung@gmail.com>
2018-06-03 14:49:49 -0700

    Add admin interface for booking slot

--- doolay/bookings/admin.py
+++ doolay/bookings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from doolay.bookings.models import BookingSlot
+
+class BookingSlotAdmin(admin.ModelAdmin):
+    list_display = ['start', 'end', 'repeat', 'repeat_until', 'notes']
+
+admin.site.register(BookingSlot, BookingSlotAdmin)
