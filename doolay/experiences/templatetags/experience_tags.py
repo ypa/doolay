84eb284b47539c5bc305e94f5e0225d5e7ae47d1
Yan P Aung <yan.pye.aung@gmail.com>
2018-08-04 06:57:07 -0700

    Make booking_calendar to display this month's days

--- doolay/experiences/templatetags/experience_tags.py
+++ doolay/experiences/templatetags/experience_tags.py
@@ -1,3 +1,5 @@
+import calendar
+from collections import defaultdict
 from django import template
 from doolay.experiences.models import ExperiencePage
 
@@ -29,4 +31,30 @@ def booking_modal(context):
 
 @register.inclusion_tag('tags/booking_calendar.html', takes_context=True)
 def booking_calendar(context):
-    return { 'request': context['request'] }
+    year, month = get_current_year_month()
+    this_month_days = get_calendar_month_days(year, month)
+    return {
+        'request': context['request'],
+        'month': month,
+        'year': year,
+        'this_month_previous': this_month_days['previous'],
+        'this_month_current': this_month_days['current'],
+        'this_month_next': this_month_days['next'],
+    }
+
+
+def get_current_year_month():
+    return (2018, 8)
+
+
+def get_calendar_month_days(year, month):
+    date_by_month = defaultdict(list)
+    cal = calendar.Calendar(calendar.SUNDAY)
+    for date in cal.itermonthdates(year, month):
+        if date.month < month:
+            date_by_month['previous'].append(date.day)
+        elif date.month == month:
+            date_by_month['current'].append(date.day)
+        else:
+            date_by_month['next'].append(date.day)
+    return date_by_month
