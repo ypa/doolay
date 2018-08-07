d87af85976a0f623c7ae51b13b6fc428fe566092
Yan P Aung <yan.pye.aung@gmail.com>
2018-08-06 20:25:15 -0700

    Toggle this and next calendars

--- doolay/static/js/main.js
+++ doolay/static/js/main.js
@@ -1,4 +1,19 @@
 
 $(document).ready(function() {
     $("#booking").datepicker();
+
+    // show this month calendar and hide the next one
+    $("#next-month-calendar").hide();
+    $("#this-month-calendar").show();
+
+    // toggle between this month and next month calendars
+    $("#this-month-calendar .btn-right").click(function() {
+  		$("#this-month-calendar").hide();
+  		$("#next-month-calendar").show();
+	});
+
+    $("#next-month-calendar .btn-left").click(function() {
+  		$("#next-month-calendar").hide();
+  		$("#this-month-calendar").show();
+	});
 });
