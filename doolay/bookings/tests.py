86f626d56d8f7c2ab91caa330c44bc8d293ee42a
Yan P Aung <yan.pye.aung@gmail.com>
2018-08-16 20:08:06 -0700

    Add booking model tests; wip

--- doolay/bookings/tests.py
+++ doolay/bookings/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.conf import settings
+from doolay.home.models import HomePage
 
 
 from model_mommy import mommy
@@ -8,4 +9,8 @@ from model_mommy.recipe import Recipe, foreign_key
 
 
 class BookingModelTest(TestCase):
-	pass
+	fixtures = ['doolay/fixtures/unittest_data.json']
+
+	def test_home(self):
+		home = HomePage.objects.first()
+		self.assertEqual(home, '')
