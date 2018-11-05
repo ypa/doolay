8dd9da40444380ad66a4f593f15101d3961f9d21
Yan P Aung <yan.pye.aung@gmail.com>
2018-11-05 07:23:28 -0800

    Validate for already requested dates

--- doolay/bookings/serializers.py
+++ doolay/bookings/serializers.py
@@ -25,15 +25,31 @@ class BookingSlotRequestSerializer(serializers.ModelSerializer):
         )
 
     def validate_request_slot_date(self, request_date):
+
+        request_date_valid = True
+
+        if self.is_request_not_available(request_date):
+            request_date_valid = False
+
+        if request_date not in self.fetch_inventory_slot_dates(request_date):
+            request_date_valid = False
+
+        if not request_date_valid:
+            raise serializers.ValidationError('The requested date is not available')
+        else:
+            return request_date_valid
+
+    def fetch_inventory_slot_dates(self, request_date):
         slot = self.context['slot']
         from_date = self.get_datetime_from_date(request_date)
         to_date = self.get_datetime_from_date(request_date, 23, 59, 59)
-        available_slots = [start_dt.date() for (start_dt, end_dt, s) in
-                           slot.all_occurrences(from_date, to_date)]
-        if request_date not in available_slots:
-            raise serializers.ValidationError('The requested date is not available')
-        else:
-            return None
+
+        return [start_dt.date() for (start_dt, end_dt, s) in
+                slot.all_occurrences(from_date, to_date)]
+
+    def is_request_not_available(self, request_date):
+        already_requested = BookingSlotRequest.objects.filter(request_date=request_date)
+        return already_requested.count() > 0
 
     def create(self, validated_data):
         validated_data['slot'] = self.context['slot']
