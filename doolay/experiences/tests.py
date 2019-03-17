2f9a04b2c730c92d100c1697df481e42dee34193
Yan P Aung <yan.pye.aung@gmail.com>
2019-03-17 08:35:24 -0700

    Refactor mock creating experience pages in unittest

--- doolay/experiences/tests.py
+++ doolay/experiences/tests.py
@@ -1,4 +1,6 @@
 
+import unittest
+from django.test import Client
 from wagtail.tests.utils import WagtailPageTests
 from doolay.home.models import HomePage
 from doolay.experiences.models import ExperiencePage, ExperienceIndexPage
@@ -8,22 +10,46 @@ from doolay.hosts.models import HostPage
 from doolay.bookings.models import Booking
 
 
-class ExperienceModelTest(WagtailPageTests):
-    fixtures = ['unittest_data.json']
-    exp_page_counter = 1 # there's already one exp page created by fixtures
+class ExpPageFactoryMixin(object):
 
-    def setUp(self):
+    def make_experience_page(self):
+        existing_page_count = ExperienceIndexPage.objects.count()
+
         host = HostPage.objects.first()
+        assert host is not None, "At least one host page is needed to create an experience page"
+
         place = PlacePage.objects.first()
-        exp_list = ExperienceIndexPage.objects.first()
-        self.exp_page_counter += 1
-        self.exp_page_recipe = Recipe(
+        assert host is not None, "At least one place page is needed to create an experience page"
+
+        return Recipe(
             ExperiencePage,
             host=host,
             place=place,
-            path='%s000%s' % (exp_list.path, self.exp_page_counter),
+            path=self.get_next_exp_page_path(),
             depth=4,
-        )
+        ).make()
+
+    def get_next_exp_page_path(self):
+        # TODO: Replace this code and instead ask wagatail for the next path
+        exp_path_width = 4
+        last_exp_page = ExperiencePage.objects.last()
+
+        if last_exp_page is not None:
+            current_path_index = int(last_exp_page.path[-exp_path_width:]) # Implicit assumptions
+            path_prefix = last_exp_page.path[:-exp_path_width] # Implicit assumptions
+        else:
+            current_path_index = 0
+            path_prefix = ExperienceIndexPage.objects.first().path # Use Index Page's path (Could potentially throw NoneType AttributeError)
+
+        next_path_index = current_path_index + 1
+        next_path_suffix = str(next_path_index).rjust(exp_path_width, '0') # padding with zeros for fixed width
+
+        return "%s%s" % (path_prefix, next_path_suffix)
+
+
+class ExperienceModelTest(WagtailPageTests, ExpPageFactoryMixin):
+
+    fixtures = ['unittest_data.json']
 
     def test_can_create_exp_page_under_exp_index_page(self):
         self.assertCanCreateAt(ExperienceIndexPage, ExperiencePage)
@@ -34,9 +60,19 @@ class ExperienceModelTest(WagtailPageTests):
     def test_creating_exp_page_also_create_booking_obj(self):
         num_bookings_before = len(Booking.objects.all())
 
-        new_exp_page = self.exp_page_recipe.make()
+        new_exp_page = self.make_experience_page()
 
         num_bookings_after = len(Booking.objects.all())
         self.assertEqual(num_bookings_after, num_bookings_before + 1)
         booking = Booking.objects.get(experience_page=new_exp_page)
         self.assertIsNotNone(booking)
+
+
+
+class ExperiencePageViewTests(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_can_navigate_to_experience_page(self):
+        pass
